c811104c328d407e2919325e5214cbdb4465886d
Joshua Trujillo <Jtriley15@gmail.com>
2022-02-09 13:08:08 -0500

    cleaned up HTML file layout

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,10 @@ fn handle_connection(mut stream: TcpStream) {
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "html/hello.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "html/hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
